a4a64188c276066e4b2e69154a9c326f3e96c7cb
Bilal Said Yılmaz <146930466+bilal-said-yilmaz@users.noreply.github.com>
2024-12-21 18:30:00 +0300

    Update Database.go

--- site/models/Database.go
+++ site/models/Database.go
@@ -1,4 +1,4 @@
 package models
 
 // var Dns = "root:root@tcp(127.0.0.1:8889)/bilaldb?charset=utf8mb4&parseTime=True&loc=Local"
-var Dsn = "postgres://postgres:Golang007@localhost:5432/blog_db?sslmode=disable"
+var Dsn = "postgres://your_postgres_username:your_user_password@localhost:5432/your_db_name?sslmode=disable"
